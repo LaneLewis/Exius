5e9aa4c2a41d209932ea737315835025b9d80385
nrd-lab-tester <molybdeunummb@gmail.com>
2022-04-12 10:31:49 -0700

    modified initiate

--- initiate.sh
+++ initiate.sh
@@ -2,5 +2,5 @@
 
 echo "Running Webdav Bash"
 nohup rclone serve webdav $CONFIGNAME:/ --addr :8081 &
-nohup rclone rcd --rc-web-gui --rc-baseurl admin --rc-user admin --rc-pass $ADMINKEY --rc-addr :8082 --rc-web-gui-no-open-browser -vv & 
+nohup rclone rcd --rc-web-gui --rc-baseurl admin --rc-user admin --rc-pass $ADMINKEY --rc-addr :8082 --rc-web-gui-no-open-browser & 
 /rclone-proxy
