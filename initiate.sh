72fcead825709d256d3c106aa58345f8dde5bd79
nrd-lab-tester <molybdeunummb@gmail.com>
2022-04-21 12:17:02 -0700

    reduced cache refresh time

--- initiate.sh
+++ initiate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Running Webdav Bash"
-nohup rclone serve webdav $CONFIGNAME:/ --addr :8081 &
+nohup rclone serve webdav $CONFIGNAME:/ --addr :8081 --dir-cache-time duration 1m0s --poll-interval duration 30s&
 nohup rclone rcd --rc-web-gui --rc-baseurl admin --rc-user admin --rc-pass $ADMINKEY --rc-addr :8082 --rc-web-gui-no-open-browser & 
 /rclone-proxy
